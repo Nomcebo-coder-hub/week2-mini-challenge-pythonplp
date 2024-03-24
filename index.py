0cf456b1c7b8293189c5e4b49daa433e2676b416
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-03-25 01:14:54 +0200

    Added dictionary showing name, age and colour

--- index.py
+++ index.py
@@ -6,3 +6,9 @@ print(books[0])
 print(books[1])
 print(books[2])
 
+name = {"What is your name?: Nomcebo"}
+print(name)
+age = {"What is your age?: 26"}
+print(age)
+favourite_color = {"What is your favourite color?: Pink"}
+print(favourite_color)
